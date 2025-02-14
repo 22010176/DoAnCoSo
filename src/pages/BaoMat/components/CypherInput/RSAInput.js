8f04f8bcd66c49a6ea599c417aafe0bb751eb3bd
Your Name <you@example.com>
2025-02-14 20:14:16 +0700

    ed

---
+++ src/pages/BaoMat/components/CypherInput/RSAInput.js
@@ -0,0 +1,75 @@
+import { useDispatch, useSelector } from "react-redux"
+
+import cypherSlice from "../../cypherSlice"
+import { getRSAInput } from "../../../../redux/selectors"
+import { isPrime } from "../../../../utilities/number"
+import { InputNumber } from "antd"
+import { checkE, findNextD, findNextE } from "../../../../utilities/rsa"
+import { useEffect, useMemo } from "react"
+
+function KeyResult({ value = [] }) {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      {/* {value.map((i,j)=>)} */}
+      <p className="text-lg">
+        ({value.join(', ')})
+      </p>
+    </div>
+
+  )
+}
+
+function RSAInput() {
+  const dispatch = useDispatch()
+  const message = JSON.parse(useSelector(getRSAInput))
+  const enableE_D = useMemo(() =>
+    isPrime(message.q) && isPrime(message.p),
+    [message.q, message.p])
+
+  function onPChange(e) {
+    dispatch(cypherSlice.actions.changeP(e))
+
+  }
+
+  function onQChange(e) {
+    dispatch(cypherSlice.actions.changeQ(e))
+  }
+
+  function onEChange(e) {
+    const _e = findNextE(message.p, message.q, e, e < message.e)
+    dispatch(cypherSlice.actions.updateInput({ e: _e, d: findNextD(message.p, message.q, _e) }))
+  }
+
+  function onDChange(e) {
+    dispatch(cypherSlice.actions.updateInput({ d: e }))
+  }
+
+  return (
+    <div className="flex gap-20">
+      <div className="grid grid-cols-[auto_1fr] gap-x-10 gap-y-3">
+        <h1 className="text-xl font-bold">P:</h1>
+        <InputNumber value={message.p} min={0} onChange={onPChange} />
+        <h1 className="text-xl font-bold">Q:</h1>
+        <InputNumber value={message.q} min={0} onChange={onQChange} />
+      </div>
+      <div className="grid grid-cols-[auto_1fr] gap-x-10 gap-y-3">
+        <h1 className="text-xl font-bold">E:</h1>
+        <InputNumber value={message.e} min={0} onChange={onEChange} disabled={!enableE_D} />
+        <h1 className="text-xl font-bold">D:</h1>
+        <InputNumber value={message.d} min={0} onChange={onDChange} disabled={!enableE_D} />
+      </div>
+      <div className="flex-grow grid grid-cols-2">
+        <div>
+          <h1 className="font-bold text-xl">Public Key</h1>
+          <KeyResult value={[1, 4]} />
+        </div>
+        <div>
+          <h1 className="font-bold text-xl">Private Key</h1>
+          <KeyResult value={[1, 4]} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RSAInput
